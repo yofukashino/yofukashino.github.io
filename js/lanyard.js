08a9802663c0dd6906fd30dee06cbc7b81c7e156
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-10-30 00:41:14 +0530

    Add files via upload

--- js/lanyard.js
+++ js/lanyard.js
@@ -1,197 +1,201 @@
-API_URL = 'https://api.lanyard.rest/v1';
-USERID = '1025214794766221384';
-
+const API_URL = 'https://api.lanyard.rest/v1';
+const USERID = '1025214794766221384';
+const pfp = document.getElementById('pfp');
+const status = document.getElementById('status');
+const statusDot = document.getElementById('status-dot');
+const status2 = document.getElementById('status2');
+const username = document.getElementById('username');
+const bigImage = document.getElementById('activity-big-image');
+const smallImage = document.getElementById('activity-small-image');
+const name = document.getElementById('activity-name');
+const state = document.getElementById('activity-state');
+const details = document.getElementById('activity-detail');
 async function fetchResponse(userId) {
     try {
-        const url = await fetch(`${API_URL}/users/${userId}`);
-        const response = await url.json();
-        return response;
-    } catch (error) {
-        console.error(error);
+        const res = await fetch(`${API_URL}/users/${userId}`);
+        return await res.json();
+    } catch (err) {
+        console.error(err);
     }
-
 }
-
-
 async function setAvatar() {
-    const response = await fetchResponse(USERID);
-
-    var avatarId = response.data.discord_user.avatar;
-    var fullUrl = `https://cdn.discordapp.com/avatars/${USERID}/${avatarId}`;
-
-    document.getElementById('pfp').src = fullUrl;
+    const {
+        data: {
+            discord_user: {
+                avatar
+            }
+        }
+    } = await fetchResponse(USERID);
+    const fullUrl = `https://cdn.discordapp.com/avatars/${USERID}/${avatar}`;
+    pfp.src = fullUrl;
 }
-
 async function setAvatarFrame() {
-    const response = await fetchResponse(USERID);
-    const activity2 = document.getElementById('status2');
-
-    if (response.data.discord_status == 'offline') {
-        activity2.innerHTML = "might be eternal sleep";
-        activity2.style.cssText = 'color: unset; opacity: 0.5;';
-    } else {
-        activity2.innerHTML = "idle";
-        activity2.style.cssText = 'color: #51d75a; opacity: 1;';
-    }
-    switch (response.data.discord_status) {
-        case 'online':
-            document.getElementById('activity-dot').style.background =
-                '#3ba45d';
-            document.getElementById('activity-dot').title = 'Online';
-            activity2.innerHTML = 'Online';
-            break;
-
-        case 'dnd':
-            document.getElementById('activity-dot').style.background =
-                '#ed4245';
-            document.getElementById('activity-dot').title = 'Do not disturb';
-            break;
-
-        case 'idle':
-            document.getElementById('activity-dot').style.background =
-                '#faa81a';
-            document.getElementById('activity-dot').title = 'Idle';
-            break;
-
-        case 'offline':
-            document.getElementById('activity-dot').style.background =
-                '#747e8c';
-            document.getElementById('activity-dot').title = 'Offline';
-            break;
+    const {
+        data: {
+            discord_status
+        }
+    } = await fetchResponse(USERID);
+    switch (discord_status) {
+    case 'online':
+        statusDot.style.background =
+            '#3ba45d';
+        statusDot.title = 'Online';
+        status2.innerHTML = 'online';
+        status2.style.cssText = 'color: #3ba45d; opacity: 1;';
+        break;
+    case 'dnd':
+        statusDot.style.background =
+            '#ed4245';
+        statusDot.title = 'Do not disturb';
+        status2.innerHTML = 'dnd';
+        status2.style.cssText = 'color: #ed4245; opacity: 1;';
+        break;
+    case 'idle':
+        statusDot.style.background =
+            '#faa81a';
+        statusDot.title = 'Idle';
+        status2.innerHTML = "idle";
+        status2.style.cssText = 'color: #faa81a; opacity: 1;';
+        break;
+    case 'offline':
+        statusDot.style.background =
+            '#747e8c';
+        statusDot.title = 'Offline';
+        status2.innerHTML = "offline <br> might be in eternal sleep";
+        status2.style.cssText = 'color: unset; opacity: 0.5;';
+        break;
     }
 }
-
 async function setUsername() {
-    const response = await fetchResponse(USERID);
-
-    var user = response.data.discord_user.username;
-    var discriminator = response.data.discord_user.discriminator;
-    var fullName = `${user}#${discriminator}`;
-
-    document.getElementById('username').innerHTML = fullName;
+    const {
+        data: {
+            discord_user: {
+                username: user,
+                discriminator
+            }
+        }
+    } = await fetchResponse(USERID);
+    username.innerHTML = `${user}&#8239;#&#8239;${discriminator}`;
 }
-
 async function setStatus() {
+    const {
+        data: {
+            discord_status,
+            activities
+        }
+    } = await fetchResponse(USERID);
+    if (discord_status == 'offline')
+        return;
+    const {
+        state
+    } = activities.find(m => m.type == 4);
+    if (!state) {
+        return;
+    }
+    status.innerHTML = `Status: "${state}"`;
+}
+async function setActivityBigImage() {
+    const {
+        data: {
+            activities,
+            spotify
+        }
+    } = await fetchResponse(USERID);
+    const mostRecent = activities.filter(m => m.type !== 4).shift();
+    if (!mostRecent?.assets) {
+        bigImage.style.display = 'none';
+        return;
+    }
+    if (mostRecent.assets.large_image.includes("spotify")) {
+        bigImage.style.display = 'block';
+        bigImage.src = spotify.album_art_url;
+        return;
+    }
+    const imageLink = `https://cdn.discordapp.com/app-assets/${mostRecent.application_id}/${mostRecent.assets.large_image}.png`;
+    bigImage.style.display = 'block';
+    bigImage.src = imageLink;
+}
+async function setActivitySmallImage() {
+    const {
+        data: {
+            activities
+        }
+    } = await fetchResponse(USERID);
+    const mostRecent = activities.filter(m => m.type !== 4).shift();
+    if (!mostRecent?.assets || mostRecent.assets.large_image.includes("spotify")) {
+        smallImage.style.display = 'none';
+        return;
+    }
+    const imageLink = `https://cdn.discordapp.com/app-assets/${mostRecent.application_id}/${mostRecent.assets.small_image}.png`;
+    smallImage.style.display = 'block';
+    smallImage.src = imageLink;
+}
+async function setActivityName() {
+    const {
+        data: {
+            activities
+        }
+    } = await fetchResponse(USERID);
+    const mostRecent = activities.filter(m => m.type !== 4).shift();
+    if (!mostRecent?.name) {
+        name.innerHTML = 'doing yo moma';
+        return;
+    }
+    name.style.display = 'block';
+    name.innerHTML = mostRecent.name;
+}
+async function setActivityState() {
+    const response = await fetchResponse(USERID);
+    const activities = response.data.activities.filter(m => m.type !== 4);
+    if (!activities.length) {
+        state.style.display = 'none';
+        return;
+    }
+    const mostRecent = activities.shift();
+    if (!mostRecent.state) {
+        state.style.display = 'none';
+        return;
+    }
+    state.style.display = 'block';
+    state.innerHTML = mostRecent.state;
+}
+async function setActivityDetails() {
     const response = await fetchResponse(USERID);
 
-    if (response.data.discord_status == 'offline') {
+    const activities = response.data.activities.filter(m => m.type !== 4);
+    if (!activities.length) {
+        details.style.display = 'none';
         return;
     }
-    var status = response.data.activities[0].state;
-    if (!status) {
+    const mostRecent = activities.shift();
+    if (!mostRecent.details) {
+        details.style.display = 'none';
         return;
     }
-    document.getElementById('status').innerHTML = `Status: "${status}"`;
+    details.style.display = 'block';
+    details.innerHTML = mostRecent.details;
 }
 
-async function setSpotifyBar() {
-    const response = await fetchResponse(USERID);
-
-    var bar = document.getElementById('spotify-innerbar');
-    var bar2 = document.getElementById('spotify-time-end');
-    var bar3 = document.getElementById('spotify-time-start');
-    
-    if (response.data.listening_to_spotify == false) {
-        bar.style.display = 'none';
-        bar2.innerHTML = '00:00';
-        bar3.innerHTML = '00:00';
-        return;
-    }
-
-    const date = new Date().getTime();
-
-    const v1 =
-        response.data.spotify.timestamps.end -
-        response.data.spotify.timestamps.start;
-    const v2 = date - response.data.spotify.timestamps.start;
-
-
-    function spotifyTimeSet(date, element) {
-        const x = document.getElementById(element);
-        const y = new Date(date);
-
-        const minutes = y.getMinutes();
-        const seconds = y.getSeconds();
-        const formmatedseconds = seconds < 10 ? `0${seconds}` : seconds;
-        x.innerHTML = `${minutes}:${formmatedseconds}`;
-    }
-
-    spotifyTimeSet(v1, 'spotify-time-end');
-    spotifyTimeSet(v2, 'spotify-time-start');
-
-    prcnt = (v2 / v1) * 100;
-
-    precentage = Math.trunc(prcnt);
-
-    prccc = Math.round((prcnt + Number.EPSILON) * 100) / 100;
-    i = 1;
-
-    bar.style.display = 'block';
-    bar.style.width = prccc + '%';
+function presenceInvoke() {
+    setActivityBigImage();
+    setActivitySmallImage();
+    setActivityName();
+    setActivityState();
+    setActivityDetails();
 }
 
-async function setSpotifySongName() {
-    const response = await fetchResponse(USERID);
-    var par = document.getElementById('spotify-song');
-
-    if (response.data.listening_to_spotify == false) {
-        par.innerHTML = 'listening to nothing';
-        return;
-    }
-
-    var songName = response.data.spotify.song;
-
-    var fullName = songName;
-    par.style.display = 'block';
-    par.innerHTML = fullName;
-}
-
-async function setSpotifyAlbumCover() {
-    const response = await fetchResponse(USERID);
-    var par = document.getElementById('album-cover');
-
-    if (response.data.listening_to_spotify == false) {
-        par.style.display = 'none';
-        return;
-    }
-
-    var albumcover = response.data.spotify.album_art_url;
-    par.style.display = 'block';
-    par.src = albumcover;
-}
-
-async function setSpotifyArtist() {
-    const response = await fetchResponse(USERID);
-    var par = document.getElementById('spotify-artist');
-
-    if (response.data.listening_to_spotify == false) {
-        par.innerHTML = 'go fuck yourself';
-        return;
-    }
-
-    var artistName = response.data.spotify.artist;
-
-    par.style.display = 'block';
-    par.innerHTML = `by: ${artistName}`;
-}
-
-async function presenceInvoke() {
-    setSpotifyAlbumCover();
-    setSpotifyArtist();
-    setSpotifySongName();
-    setSpotifyBar();
-}
-
-async function statusInvoke() {
+function statusInvoke() {
     setStatus();
     setAvatarFrame();
 }
 
-async function invoke() {
-    setInterval(presenceInvoke, 1000);
+function invoke() {
+    setInterval(() => {
+        presenceInvoke();
+        statusInvoke();
+    }, 1000);
     setAvatar();
     setUsername();
-    setInterval(statusInvoke, 1000);
 }
 
 invoke();
