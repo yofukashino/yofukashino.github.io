80e9c06b4a773841c92737740a72974315577fd2
Tharki-God <alokender018@gmail.com>
2023-07-01 08:43:22 +0530

    react commit

---
+++ src/assets/connections/index.js
@@ -0,0 +1,11 @@
+import bot from "./bot.png";
+import discord from "./discord.svg";
+import github from "./github.svg";
+import insta from "./insta.png";
+import plugin from "./plugin.png";
+import support from "./support.png";
+import twitch from "./twitch.png";
+import youtube from "./youtube.png";
+const connections = {bot, discord, github, insta, plugin, support, twitch, youtube};
+
+export default connections;
---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import background from "./background.gif";
+import loader from "./loader.gif";
+import connections from "./connections"
+const assets = {
+    background,
+    loader,
+    connections
+};
+export default assets;
