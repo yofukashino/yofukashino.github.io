41534c8e8aa8eab1de40c065ea9d13f40bbd43ce
Tharki-God <alokender018@gmail.com>
2024-03-01 07:19:37 +0530

    uwu

--- src/LanyardProfile/Components/Connections/index.tsx
+++ src/LanyardProfile/Components/Connections/index.tsx
@@ -16,25 +16,25 @@ export default React.memo(() => {
       </span>
       {shown && (
         <>
-          <a data-tooltip="github" href="https://github.com/tharkidev">
+          <a data-tooltip="github" href="https://github.com/yofukashino">
             <Assets.connections.github className="connection-icon" />
           </a>
-          <a data-tooltip="loneweeb" href="https://discord.com/users/1025214794766221384/">
+          <a data-tooltip="loneweeb" href="https://discord.com/users/1121961711080050780/">
             <Assets.connections.discord className="connection-icon" />
           </a>
           <a data-tooltip="support server" href="https://discord.com/invite/SgKSKyh9gY">
             <Assets.connections.support className="connection-icon" />
           </a>
-          <a data-tooltip="rp plugins" href="https://github.com/tharkidev/RepluggedPlugins">
+          <a data-tooltip="rp plugins" href="https://github.com/yofukashino/RepluggedPlugins">
             <Assets.connections.plugin className="connection-icon" />
           </a>
-          <a data-tooltip="instagram" href="https://www.instagram.com/tharkiahlawat/">
+          <a data-tooltip="instagram" href="https://www.instagram.com/yofukashino/">
             <Assets.connections.insta className="connection-icon" />
           </a>
           <a data-tooltip="youtube" href="https://www.youtube.com/channel/UCYQnV4Z-TOVuyBVWjkBKZtg">
             <Assets.connections.youtube className="connection-icon" />
           </a>
-          <a data-tooltip="twitch" href="https://www.twitch.tv/tharkiahlawat">
+          <a data-tooltip="twitch" href="https://www.twitch.tv/yofukashi_no">
             <Assets.connections.twitch className="connection-icon" />
           </a>
         </>
