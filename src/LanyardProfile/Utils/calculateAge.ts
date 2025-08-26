08ba5771cd7935c03a7f5e4c761180757185e00b
yofukashino <alokender018@gmail.com>
2025-08-26 12:10:55 +0530

    chore: prettier

--- src/LanyardProfile/Utils/calculateAge.ts
+++ src/LanyardProfile/Utils/calculateAge.ts
@@ -1,7 +1,8 @@
 export default ({ date, month, year }: { date: number; month: number; year: number }): number => {
   // https://jsdate.wtf
   const dob = new Date(year, month - 1, date);
-  const msDifference = Date.now() - dob.getTime();
+  const today = new Date();
+  const msDifference = today.getTime() - dob.getTime();
   const ageDate = new Date(msDifference);
   return Math.abs(ageDate.getUTCFullYear() - 1970);
 };
