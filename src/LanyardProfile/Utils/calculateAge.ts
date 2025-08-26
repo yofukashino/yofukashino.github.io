6340fc046d1e1bf0bc349eefc11a4b18837bd4b5
yofukashino <alokender018@gmail.com>
2025-08-26 11:49:19 +0530

    chore: year based rather than ms

--- src/LanyardProfile/Utils/calculateAge.ts
+++ src/LanyardProfile/Utils/calculateAge.ts
@@ -1,7 +1,6 @@
 export default ({ date, month, year }: { date: number; month: number; year: number }): number => {
-  const dob = new Date(year, month, date);
-  const msDifference = Date.now() - dob.getTime();
-  const ageDate = new Date(msDifference);
-
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
+  const currDate = new Date();
+  const birthDate = new Date(year, month, date);
+  const yearDifference = currDate.getUTCFullYear() - birthDate.getUTCFullYear();
+  return Math.abs(yearDifference);
 };
